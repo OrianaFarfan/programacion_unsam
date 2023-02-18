a9c94d2798443827a8c10ae421234d94bb974f99
Oriana Farfán <85259701+OrianaFarfan@users.noreply.github.com>
2023-02-18 18:26:34 -0300

    Update mem_dina2.c

--- mem_dina2.c
+++ mem_dina2.c
@@ -1,7 +1,4 @@
 /*
-Programacion UNSAM
-Autor: Carlos Douglas Rodriguez
-Año: 2020
 2. Escribir un programa para leer el archivo vector.dat del ejercicio anterior y
 almacenarlo en un vector. No se conoce la cantidad de elementos y no se puede
 limitar
